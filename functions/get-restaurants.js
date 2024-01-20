219229cbbba29aefb4a7c975931528c79493edfa
kshyun28 <jasper.d.gabriel@gmail.com>
2024-01-20 17:33:35 +0800

    refactor: use SSM for configs

--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,9 +1,12 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb")
 const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb")
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
+
 const dynamodbClient = new DynamoDB()
 const dynamodb = DynamoDBDocumentClient.from(dynamodbClient)
 
-const defaultResults = parseInt(process.env.default_results)
+const { serviceName, stage } = process.env
 const tableName = process.env.restaurants_table
 
 const getRestaurants = async (count) => {
@@ -17,12 +20,19 @@ const getRestaurants = async (count) => {
   return resp.Items
 }
 
-module.exports.handler = async (event, context) => {
-  const restaurants = await getRestaurants(defaultResults)
+module.exports.handler = middy(async (event, context) => {
+  const restaurants = await getRestaurants(context.config.defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
   }
 
   return response
-}
+}).use(ssm({
+  cache: true,
+  cacheExpiry: 1 * 60 * 1000, // 1 mins
+  setToContext: true,
+  fetchData: {
+    config: `/${serviceName}/${stage}/get-restaurants/config`
+  }
+}))
--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,9 +1,12 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb")
 const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb")
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
+
 const dynamodbClient = new DynamoDB()
 const dynamodb = DynamoDBDocumentClient.from(dynamodbClient)
 
-const defaultResults = parseInt(process.env.default_results)
+const { serviceName, stage } = process.env
 const tableName = process.env.restaurants_table
 
 const findRestaurantsByTheme = async (theme, count) => {
@@ -19,14 +22,21 @@ const findRestaurantsByTheme = async (theme, count) => {
   return resp.Items
 }
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = middy(async (event, context) => {
   const req = JSON.parse(event.body)
   const theme = req.theme
-  const restaurants = await findRestaurantsByTheme(theme, defaultResults)
+  const restaurants = await findRestaurantsByTheme(theme, context.config.defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
   }
 
   return response
-}
+}).use(ssm({
+  cache: true,
+  cacheExpiry: 1 * 60 * 1000, // 1 mins
+  setToContext: true,
+  fetchData: {
+    config: `/${serviceName}/${stage}/search-restaurants/config`
+  }
+}))
