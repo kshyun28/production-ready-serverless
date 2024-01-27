2022fd2ac23cc9ba35f7e47fbca474cbd3cb0be9
kshyun28 <jasper.d.gabriel@gmail.com>
2024-01-27 15:57:16 +0800

    feat: add API Gateway metrics and custom metrics

--- functions/place-order.js
+++ functions/place-order.js
@@ -1,6 +1,13 @@
 const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge')
 const eventBridge = new EventBridgeClient()
 const chance = require('chance').Chance()
+const { Metrics, MetricUnits } = require('@aws-lambda-powertools/metrics');
+
+const metrics = new Metrics({
+  namespace: 'big-mouth',
+  serviceName: 'orders',
+  defaultDimensions: { environment: process.env.stage },
+});
 
 const busName = process.env.bus_name
 
@@ -22,6 +29,9 @@ module.exports.handler = async (event) => {
     }]
   })
   await eventBridge.send(putEvent)
+
+  metrics.addMetric('orderPlaced', MetricUnits.Count, 1);
+  metrics.publishStoredMetrics();
 
   console.log(`published 'order_placed' event into EventBridge`)
 
